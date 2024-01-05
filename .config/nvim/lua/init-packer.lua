ab3d9de7c56facce03d80291713aca056f7b5c6a
Rocktim Saikia <rocktimthedev@gmail.com>
2024-01-06 02:58:24 +0530

    refactor(nvim-config): enhance indent-blankline setup

--- .config/nvim/lua/init-packer.lua
+++ .config/nvim/lua/init-packer.lua
@@ -220,7 +220,33 @@ return require("packer").startup(function(use)
     use({
         "lukas-reineke/indent-blankline.nvim",
         config = function()
-            require("ibl").setup()
+            local hooks = require("ibl.hooks")
+            -- create the highlight groups in the highlight setup hook, so they are reset
+            -- every time the colorscheme changes
+            hooks.register(hooks.type.HIGHLIGHT_SETUP, function()
+                vim.api.nvim_set_hl(0, "RainbowRed", { fg = "#E06C75" })
+                vim.api.nvim_set_hl(0, "RainbowYellow", { fg = "#E5C07B" })
+                vim.api.nvim_set_hl(0, "RainbowBlue", { fg = "#61AFEF" })
+                vim.api.nvim_set_hl(0, "RainbowOrange", { fg = "#D19A66" })
+                vim.api.nvim_set_hl(0, "RainbowGreen", { fg = "#98C379" })
+                vim.api.nvim_set_hl(0, "RainbowViolet", { fg = "#C678DD" })
+                vim.api.nvim_set_hl(0, "RainbowCyan", { fg = "#56B6C2" })
+                vim.api.nvim_set_hl(0, "Lowcontrast", { fg = "#232a2d" }) -- Taken from  Everblush kitty theme's color0
+            end)
+
+            require("ibl").setup({
+                indent = {
+                    char = "▏",
+                    -- char = "",
+                    highlight = { "Lowcontrast" },
+                },
+                scope = {
+                    enabled = false,
+                    highlight = {
+                        "@function",
+                    },
+                },
+            })
         end,
     })
 end)
