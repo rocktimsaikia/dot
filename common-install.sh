fa6b647f4bc0a8c2e182b22657976179cb8a6d02
Rocktim Saikia <rocktimthedev@gmail.com>
2024-01-11 21:15:18 +0530

    style: quote program names in install checks

--- common-install.sh
+++ common-install.sh
@@ -7,7 +7,7 @@ command_exists() {
 # Install bat | A better cat
 # https://github.com/sharkdp/bat
 if command_exists bat; then
-	echo "- bat is already installed ✅ Skipping.."
+	echo '- "bat" is already installed ✅ Skipping..'
 else
 	sudo apt install bat
 	mkdir -p ~/.local/bin
@@ -17,7 +17,7 @@ fi
 # Install starship | A better prompt
 # https://starship.rs/guide/
 if command_exists starship; then
-	echo "- starship is already installed ✅ Skipping.."
+	echo '- "starship" is already installed ✅ Skipping..'
 else
 	curl -sS https://starship.rs/install.sh | sh
 	echo 'eval "$(starship init zsh)"' >> ~/.config/zsh/.zshrc
@@ -25,7 +25,13 @@ fi
 
 # Install n | Simple Node version manager
 if command_exists n; then
-	echo "- n is already installed ✅ Skipping.."
+	echo '- "n" is already installed ✅ Skipping..'
 else
 	curl -L https://bit.ly/n-install | bash
+fi
+
+if command_exists cargo; then
+	echo '- "cargo" is already installed ✅ Skipping..'
+else
+	curl https://sh.rustup.rs -sSf | sh
 fi
