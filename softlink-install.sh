44dbf26b83ca2e7c484a4f735661a0416e0b78f6
Rocktim Saikia <rocktimthedev@gmail.com>
2024-01-11 21:33:17 +0530

    chore: streamline softlink creation in install script

--- softlink-install.sh
+++ softlink-install.sh
@@ -2,10 +2,6 @@
 
 CURRENT_DIR="$PWD"
 
-# Custom scripts
-printf "\nCreating softlink for custom scripts...\n"
 ln -s "$CURRENT_DIR/scripts" "$HOME/scripts"
-
-# Neovim
-printf "\nCreating softlink for Neovim...\n"
-ln -s "$HOME/dot/.config/nvim" "$HOME/.config/nvim"
+ln -s "$CURRENT_DIR/.config/nvim" "$HOME/.config/nvim"
+ln -s "$CURRENT_DIR/.gitconfig" "$HOME/.gitconfig"
