7e15d7062fe159c40f04b981f2ba87b15c2fe4b2
Rocktim Saikia <rocktimthedev@gmail.com>
2024-01-11 19:39:37 +0530

    chore(common-install): consolidate starship install script

--- common-install.sh
+++ common-install.sh
@@ -1,11 +1,21 @@
 #!/bin/bash
 
 # Install bat | A better cat
+# https://github.com/sharkdp/bat
 if [ -x "$(command -v bat)"  ]; then
-	echo "bat is already installed ✅ Skipping.."
-	exit 0
+	echo "- bat is already installed ✅ Skipping.."
 else
 	sudo apt install bat
 	mkdir -p ~/.local/bin
 	ln -s /usr/bin/batcat ~/.local/bin/bat
 fi
+
+# Install starship | A better prompt
+# https://starship.rs/guide/
+if [ -x "$(command -v starship)"  ]; then
+	echo "- starship is already installed ✅ Skipping.."
+else
+	curl -sS https://starship.rs/install.sh | sh
+	echo 'eval "$(starship init zsh)"' >> ~/.config/zsh/.zshrc
+fi
+
--- starship-install.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-curl -sS https://starship.rs/install.sh | sh
-echo 'eval "$(starship init zsh)"' >> ~/.config/zsh/.zshrc
