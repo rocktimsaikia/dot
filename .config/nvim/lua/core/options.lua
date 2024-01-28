fb14e4212fca45c50394e174a4f81e22efe8f984
Rocktim Saikia <rocktimthedev@gmail.com>
2024-01-28 12:42:46 +0530

    refactor(nvim-config): use vim.opt and vim.g consistently for options

--- .config/nvim/lua/core/options.lua
+++ .config/nvim/lua/core/options.lua
@@ -1,7 +1,4 @@
-local g = vim.g
-local opt = vim.opt
-
-opt.ttimeoutlen = -1
+vim.opt.ttimeoutlen = -1
 
 vim.opt.tabstop = 2 -- Set the number of spaces that a <Tab> in the file counts for.
 vim.opt.shiftwidth = 2 -- Set the number of spaces to use for each step of (auto)indent.
@@ -9,45 +6,38 @@ vim.opt.expandtab = false -- Use tabs, not spaces.
 vim.opt.smarttab = true -- Insert tabs on the start of a line according to shiftwidth.
 
 -- General settings
-opt.number = true
-opt.relativenumber = true
+vim.opt.number = true
+vim.opt.relativenumber = true
 
-opt.scrolloff = 8
-opt.termguicolors = true
+vim.opt.scrolloff = 8
+vim.opt.termguicolors = true
 
 -- Search settings
-opt.ignorecase = true
-opt.smartcase = true
-opt.swapfile = false
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+vim.opt.swapfile = false
 
 -- Cursor line
-opt.cursorline = true
+vim.opt.cursorline = true
 
 --  Makes life easier
-opt.iskeyword:append("-")
+vim.opt.iskeyword:append("-")
 
 -- Open vsplit to right
-opt.splitright = true
-opt.splitbelow = true
+vim.opt.splitright = true
+vim.opt.splitbelow = true
 
-opt.hidden = true
+vim.opt.hidden = true
 
-g.git_messenger_floating_win_opts = { border = "rounded" }
-g.git_messenger_popup_content_margins = false
+vim.g.git_messenger_floating_win_opts = { border = "rounded" }
+vim.g.git_messenger_popup_content_margins = false
 
--- opt.list = true
--- opt.listchars:append("eol:↴")
--- opt.listchars:append("space:⋅")
-
--- opt.spell = true
--- opt.spelllang = "en_us"
-
-g.copilot_filetypes = { markdown = true, yml = true }
+vim.g.copilot_filetypes = { markdown = true, yml = true }
 
 -- Fix spell error by <C-x-s>
-opt.spell = true
-opt.spelllang = "en"
+vim.opt.spell = true
+vim.opt.spelllang = "en"
 
-g.doge_doc_standard_python = "numpy"
-g.doge_enable_mappings = 0
-g.doge_buffer_mappings = 0
+vim.g.doge_doc_standard_python = "numpy"
+vim.g.doge_enable_mappings = 0
+vim.g.doge_buffer_mappings = 0
